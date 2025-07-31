46693ba145fead38ab2237cee4c6e6bea6707c3f
Subhojyoti Dey <sdtheviners@gmail.com>
2025-07-31 23:45:46 +0530

    New version uploaded

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # config.py
 
-input_excel = 'I:\\My Drive\\HireThen\\Internal - Flyberg Content\\Location Scrapping Info\\Shreya Dey for photos\\FINAL Albuquerque.xlsx'
-last_processed_row = 1078
+input_excel = 'G:\\My Drive\\HireThen\\Internal - Flyberg Content\\Location Scrapping Info\\Shreya Dey for photos\\FINAL Charlotte.xlsx'
+last_processed_row = 1155
 max_images_per_site = 10
 min_image_size = [250, 250]
-output_dir = 'I:\\My Drive\\HireThen\\Internal - Flyberg Content\\POI BreakOut Pictures\\TEST 2.0'
-log_excel = 'C:\\Users\\Misty\\Desktop\\Logger.xlsx'
-summary_json = 'summary.json'
-download_concurrency = 10
+output_dir = 'G:\\My Drive\\HireThen\\Internal - Flyberg Content\\POI BreakOut Pictures\\Charlotte'
+log_excel = 'H:\\My Drive\\Python - Subhojyoti\\Image Scraper (No Filter) 2.0\\Logs\\Charlotte_Log.xlsx'
+summary_json = 'Logs\\Summaries\\summary_Charlotte.json'
+download_concurrency = 4
 request_retries = 1
 timeout = 10
 log_level = 'ERROR'
-executor_workers = 10
-instances = 1
+executor_workers = 4
+instances = 6
